7de7e24d60b770436cdef10717566b5df4b2676b
Violetta Zakorzhevskaya <vzakorz@gmail.com>
2024-09-18 16:37:54 +0300

    ## Refaktoreeritud administraatori kontroller ja uuendatud administraatori rühmade vaade
    - **controllers/admin.php**
      - Eemaldatud liigsed kommentaarid ja tõlked funktsioonis `validatePersonalCode`.
      - Lihtsustatud grupi päringut funktsioonis `groups`, eemaldades mittevajaliku `subjectCount`.
      - Üldine puhastamine ja optimeerimine administraatori meetodites selguse huvides.
    
    - **views/admin/admin_groups.php**
      - Eemaldatud "Ainete arv" ja "Tegevused" veerud grupi tabelist.
      - Lihtsustatud tabeli interaktsioone, muutes `<tr data-href>` lähenemise puhtamaks `onclick` sündmuseks ridade navigeerimiseks.

--- views/admin/admin_groups.php
+++ views/admin/admin_groups.php
@@ -2,27 +2,23 @@
     #subject-table th {
         background-color: #f2f2f2;
     }
+    #subject-table tr {
+        cursor: pointer;
+    }
 </style>
 <div class="row">
-
-    <h1>Gruppid</h1>
+    <h1>Grupid</h1>
     <div class="table-responsive">
         <table id="subject-table" class="table table-bordered">
             <thead>
             <tr>
-                <th>Grupp</th>
-                <th>Ainete arv</th>
-                <th>Tegevused</th>
+                <th>Nimi</th>
             </tr>
             </thead>
             <tbody>
             <?php foreach ($groups as $group): ?>
-                <tr data-href="subjects/<?= $group['groupId'] ?>">
+                <tr onclick="window.location.href='admin/groups/<?= $group['groupId'] ?>'">
                     <td><b><?= $group['groupName'] ?></b></td>
-                    <td><b><?= $group['subjectCount'] ?></b></td>
-                    <td>
-                        <a href="gropus/<?= $group['groupId'] ?>">Vaata</a>
-                    </td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
