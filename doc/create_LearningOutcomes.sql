86dd79a91e0d5eaf577012114292797fc9be2ccd
MrKnights1 <eero.vallistu@vikk.ee>
2025-07-14 12:05:37 +0300

    remove commented-out old table definition from create_LearningOutcomes.sql

--- doc/create_LearningOutcomes.sql
+++ doc/create_LearningOutcomes.sql
@@ -5,17 +5,3 @@ CREATE TABLE LearningOutcomes (
   nameEt VARCHAR(255) NOT NULL,
   learningOutcomeOrderNr INT DEFAULT NULL
 );
-
-
-
-/*
-OLD
-CREATE TABLE LearningOutcomes (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  subjectId INT NOT NULL,
-  outcomeId INT NOT NULL,
-  outcomeName VARCHAR(255) NOT NULL,
-  learningOutcomeOrderNr INT DEFAULT NULL,
-  syncedAt DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-*/
