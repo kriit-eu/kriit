2daaf299c00dbb056ca9adff48dd766faf28a58b
MrKnights1 <eero.vallistu@vikk.ee>
2025-09-17 14:29:53 +0300

    Rename plannedHours to subjectPlannedHours in subjects table schema for clarity

--- doc/add_planned_hours_to_subjects.sql
+++ doc/add_planned_hours_to_subjects.sql
@@ -1,2 +1,2 @@
--- Add plannedHours column to subjects table
-ALTER TABLE subjects ADD COLUMN plannedHours SMALLINT UNSIGNED NULL DEFAULT NULL COMMENT 'Planned independent work hours for the subject';
+-- Add subjectPlannedHours column to subjects table
+ALTER TABLE subjects ADD COLUMN subjectPlannedHours SMALLINT UNSIGNED NULL DEFAULT NULL COMMENT 'Planned independent work hours for the subject';
